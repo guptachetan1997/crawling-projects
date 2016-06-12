3127780ab7bbcb369a913c42fcb1117c20499be4
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-06-12 12:54:04 +0530

    twitterapi

--- Twitter API test/twitter_api.py
+++ Twitter API test/twitter_api.py
@@ -1,20 +1,25 @@
 import twitter
 import csv
 
-oauth_object = []
+oauth_object = ["", "", "", ""]
 file = open("oauth.datas", 'r')
-for row in file:
-	oauth_object.append(str(row))
+for row,i in zip(file,range(0,4)):
+    oauth_object[i] = row[:len(row)-1]
 
 file.close()
+
+# oauth_object[0] = '5AVMLWLoq0vMWMwaOi80PRzLR'
+# oauth_object[1] = 'hjDs8amMcyWBCQhIQX9kWPJ6TfjVXKSCzg6GNZIKr1QpCxCEd4'
+# oauth_object[2] = '384641534-EiZ0Snu1fJdtTs4qe1COc2MloZvZ1G5houEIdsVr'
+# oauth_object[3] = '79Qrfx504I5bLgyE9S4drZDG6m80djYb0vqAQuChB5rpW'
 
 api = twitter.Api(consumer_key=oauth_object[0],
                       consumer_secret=oauth_object[1],
                       access_token_key=oauth_object[2],
                       access_token_secret=oauth_object[3])
 
-
-tweets = api.GetSearch(term = "earthquake", count=10)
-# print tweets
+# print(api.VerifyCredentials())
+tweets = api.GetSearch(term="AAP", count=10, lang="en")
 for tweet in tweets:
-	print tweet.text
+	print (tweet.text).encode('utf8')
+
--- experiment.py
+++ experiment.py
@@ -1,6 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+def get_requests(url):
+    r = requests.get(url)
+    r.raise_for_status()
+    html = r.text.encode('utf8')
+    soup = BeautifulSoup(html)
+    print soup
+    
+
 main():
     r = requests.get("http://forum.ucweb.com/forum.php?mod=forumdisplay&fid=88&page=4")
     html = r.text.encode('utf8')
