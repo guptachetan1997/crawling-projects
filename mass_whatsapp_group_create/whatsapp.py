b0f40fe40d484de795c5615cb1fb528588c8f33a
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-08-03 07:29:27 +0530

    Mass Whatsapp Group Creater

---
+++ mass_whatsapp_group_create/whatsapp.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+print('Enter batch name : ')
+batch = input()
+
+with open(batch+'.txt', 'rb') as file:
+	lines = file.read().splitlines()
+	length = len(lines) + 1
+	df = pd.DataFrame()
+	df['First Name'] = 4
+	fnames = ['Target'+str(x) for x in range(1,length)]
+	df['First Name'] = pd.Series(fnames)
+	df['Last Name'] = 4
+	lnames = [batch for x in range(1,length)]
+	df['Last Name'] = pd.Series(lnames)
+	fnames = [str(x) for x in range(1,length)]
+	nums = [line.decode('utf8') for line in lines]
+	df['Home Phone'] = 4
+	df['Home Phone'] = pd.Series(nums)
+	df['Display Name'] = df['First Name'] + df['Last Name']
+	print(df)
+	df.to_csv(batch + '.csv', index=False)
